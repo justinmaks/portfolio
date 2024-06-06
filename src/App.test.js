7ade3b05e7159e583b2d912d8930c9bfed190d2b
justinmaks79@gmail.com <justinmaks79@gmail.com>
2024-06-06 01:16:29 -0400

    update tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
+// src/App.test.js
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders navigation links', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const homeLink = screen.getByText(/home/i);
+  const toolsLink = screen.getByText(/tools/i);
+  const snakeLink = screen.getByText(/snake/i);
+  
+  expect(homeLink).toBeInTheDocument();
+  expect(toolsLink).toBeInTheDocument();
+  expect(snakeLink).toBeInTheDocument();
 });
